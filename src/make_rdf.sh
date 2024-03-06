1f5809ef62ada5a98bc1d3510fd79ba7fd0eb4df
jmillanacosta <javier.millan.acosta@gmail.com>
2024-03-06 16:02:03 +0100

    Some progress with mappings

--- src/make_rdf.sh
+++ src/make_rdf.sh
@@ -9,12 +9,12 @@ RML="src/mappings/$1.rml"
 RDF="data/RDF/$1.ttl"
 
 # Step 2: Download RML Mapper
-echo "Downloading RML Mapper v6.2.2"
-wget -nc https://github.com/RMLio/rmlmapper-java/releases/download/v6.2.2/rmlmapper-6.2.2-r371-all.jar
+#echo "Downloading RML Mapper v6.2.2"
+#wget -nc https://github.com/RMLio/rmlmapper-java/releases/download/v6.2.2/rmlmapper-6.2.2-r371-all.jar
 
 # Step 3: Install YARRRML Parser
-echo "Installing YARRRML Parser (npm i @rmlio/yarrrml-parser -g)"
-npm list yarrrml-parser | npm install -g @rmlio/yarrrml-parser
+#echo "Installing YARRRML Parser (npm i @rmlio/yarrrml-parser -g)"
+#npm list yarrrml-parser | npm install -g @rmlio/yarrrml-parser
 
 echo ______________________________________
 # Step 4: Run YARRRML to obtain RML
