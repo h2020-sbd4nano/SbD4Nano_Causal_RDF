4a0e5e5f728c9c1aae78eef27e39d1bd6057f26f
jmillanacosta <javier.millan.acosta@gmail.com>
2024-03-06 16:13:07 +0100

    Also n-quads

--- src/make_rdf.sh
+++ src/make_rdf.sh
@@ -6,7 +6,8 @@
 # Step 1: define paths
 YARRRML="src/mappings/$1.yarrrml.yml"
 RML="src/mappings/$1.rml"
-RDF="data/RDF/$1.ttl"
+TTL="data/RDF/$1.ttl"
+NQ="data/RDF/$1.nq"
 
 # Step 2: Download RML Mapper
 #echo "Downloading RML Mapper v6.2.2"
@@ -22,9 +23,10 @@ echo "Running YARRRML on the YAML file to generate RML: $RML"
 yarrrml-parser --input $YARRRML > $RML
 
 # Step 5: Run RML Mapper
-echo "Running RML Mapper to generate RDF triples"
-java -jar rmlmapper-6.2.2-r371-all.jar -m $RML -o $RDF -s turtle
-
+echo "Running RML Mapper to generate RDF ttl"
+java -jar rmlmapper-6.2.2-r371-all.jar -m $RML -o $TTL -s turtle
+echo "Running RML Mapper to generate RDF nq"
+java -jar rmlmapper-6.2.2-r371-all.jar -m $RML -o $NQ
 # Step 6: cleanup
 echo "Removing files"
 #rm rmlmapper*
